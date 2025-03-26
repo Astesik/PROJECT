60b2e831e23241e1473c79ab157f07528fe5e2cc
Jakub Sikora <57323228+Astesik@users.noreply.github.com>
2025-03-26 12:03:28 +0100

    Update DatabaseConnection.java

--- src/main/java/com/example/project/database/DatabaseConnection.java
+++ src/main/java/com/example/project/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://127.0.0.1:5432/car_rental";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Kuba1234!";
+    private static final String PASSWORD = "###";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
